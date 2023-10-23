3a439bf73d727b5cccbed99c69ba46fa04212104
ClotaireENSC <cmardon001@ensc.fr>
2023-10-23 15:36:47 +0200

    Erreur maj bdd

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿Todo todo1 = new Todo(4, "Wash the dishes", true);
 Todo todo2 = new Todo(5, "Clean the house", false);
 Todo todo3 = new Todo(6, "Mow the lawn", false);
+
+TodoContext context = new TodoContext();
+
+context.Todo.Add(todo1);
+context.Todo.Add(todo2);
+context.Todo.Add(todo3);
+context.SaveChanges();
