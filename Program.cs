4b357bc6b99fc3ba15b4771b83990b68107f453f
ClotaireENSC <cmardon001@ensc.fr>
2023-10-23 15:53:15 +0200

    affichage des todos

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
-﻿Todo todo1 = new Todo(4, "Wash the dishes", true);
-Todo todo2 = new Todo(5, "Clean the house", false);
-Todo todo3 = new Todo(6, "Mow the lawn", false);
+﻿// Todo todo1 = new Todo(4, "Wash the dishes", true);
+// Todo todo2 = new Todo(5, "Clean the house", false);
+// Todo todo3 = new Todo(6, "Mow the lawn", false);
 
 TodoContext context = new TodoContext();
 
-context.Todo.Add(todo1);
-context.Todo.Add(todo2);
-context.Todo.Add(todo3);
-context.SaveChanges();
+// context.Todo.Add(todo1);
+// context.Todo.Add(todo2);
+// context.Todo.Add(todo3);
+// context.SaveChanges();
+
+Console.WriteLine("==== Tous les Todos ====");
+foreach (var todo in context.Todo)
+{
+    Console.WriteLine($"Todo {todo.Id}, task: {todo.Task}, completed: {todo.Completed}");
+}
+
+Console.WriteLine("==== Tous les Todos non terminés ====");
+var notCompletedTodo = context.Todo.Where(x => x.Completed == false);
+foreach (var todo in notCompletedTodo)
+{
+    if (!todo.Completed)
+        Console.WriteLine($"Todo {todo.Id}, task: {todo.Task}, completed: {todo.Completed}");
+}
+
